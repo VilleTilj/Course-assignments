3d022d9942cf854635410a15c484422a1e1b700f
Tiljander Ville <ville.tiljander@tuni.fi>
2022-03-21 14:05:50 +0200

    Add obstacle detection controller.

--- Fundamentals_of_mobile_robots/Collision_Avoidance_with_SI_model/SI_model_avoid_ball.py
+++ Fundamentals_of_mobile_robots/Collision_Avoidance_with_SI_model/SI_model_avoid_ball.py
@@ -2,32 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from visualize_mobile_robot import sim_mobile_robot
 from detect_obstacle import detect_obstacle_e5t2
+import math
 
 # Constants and Settings
 Ts = 0.01 # Update simulation every 10ms
 t_max = np.pi # total simulation duration in seconds
 # Set initial state
 init_state = np.array([-2., -.5, 0.]) # px, py, theta
+obstacle_center_point = np.array([0., 0., 0.])
 IS_SHOWING_2DVISUALIZATION = True
 
 # Define Field size for plotting (should be in tuple)
 field_x = (-2.5, 2.5)
 field_y = (-2, 2)
-
-
-# Compute the positions that is detected by the range sensor
-# to be used in visualization and in control algorithm
-def compute_sensor_endpoint(robot_state, sensors_dist):
-
-    # NOTE: we assume sensor position is in the robot's center
-    sens_N = len(sensors_dist)
-    obst_points = np.zeros((3,sens_N))
-
-    for i in range(sens_N):
-        obst_points[:,i] = robot_state + 0.1*i*np.ones(3) # NOTE: this is dummy value
-        # TODO: do proper calculation
-
-    return obst_points[:2,:] # only return x and y values
 
 
 # MAIN SIMULATION COMPUTATION
@@ -37,7 +24,7 @@ def simulate_control():
 
     # Initialize robot's state (Single Integrator)
     robot_state = init_state.copy() # numpy array for [px, py, theta]
-    desired_state = np.array([2., 0., 0.]) # numpy array for goal / the desired [px, py, theta]
+    desired_state = np.array([2., 1., 0.]) # numpy array for goal / the desired [px, py, theta]
     current_input = np.array([0., 0., 0.]) # initial numpy array for [vx, vy, omega]
 
     # Store the value that needed for plotting: total step number x data length
@@ -51,41 +38,32 @@ def simulate_control():
         sim_visualizer.set_field( field_x, field_y ) # set plot area
         sim_visualizer.show_goal(desired_state)
         # ADD OBJECT TO PLOT
-        d_safe = 0.5
+        circle_r = 0.5
         eps = 0.2
-        sim_visualizer.ax.add_patch( plt.Circle( (0,0), 0.5, color="red"))
-        sim_visualizer.ax.add_patch( plt.Circle( (0,0), d_safe, color="red",  fill=False))
-        sim_visualizer.ax.add_patch( plt.Circle( (0,0), d_safe + eps, color="green",fill=False))
+        d_safe = circle_r + eps
+        sim_visualizer.ax.add_patch( plt.Circle( (0,0), circle_r, color="red"))
+        sim_visualizer.ax.add_patch( plt.Circle( (0,0), circle_r, color="red",  fill=False))
+        sim_visualizer.ax.add_patch( plt.Circle( (0,0), d_safe, color="green",fill=False))
  
-
-        # get sensor reading
-        sensors_dist = detect_obstacle_e5t2( robot_state[0], robot_state[1], robot_state[2]) 
-        # compute and plot sensor reading endpoint
-        obst_points = compute_sensor_endpoint(robot_state, sensors_dist)
-        pl_sens, = sim_visualizer.ax.plot(obst_points[0], obst_points[1], '.', marker='X')
-        pl_txt = [sim_visualizer.ax.text(obst_points[0,i], obst_points[1,i], str(i)) for i in range(len(sensors_dist))]
-
 
     for it in range(sim_iter):
         # record current state at time-step t
         state_history[it] = robot_state
         goal_history[it] = desired_state
 
-        # Get information from sensors
-        sensors_dist = detect_obstacle_e5t2( robot_state[0], robot_state[1], robot_state[2]) 
-        # compute and plot sensor reading endpoint
-        obst_points = compute_sensor_endpoint(robot_state, sensors_dist)
-
         # IMPLEMENTATION OF CONTROLLER
         #------------------------------------------------------------
         # Compute the control input 
-        current_input[0] = 0
-        current_input[1] = 0
-        current_input[2] = -2.
-
         # TODO: change the implementation to switching
         #------------------------------------------------------------
-
+        if np.linalg.norm(robot_state - obstacle_center_point) < d_safe:
+            k = calculate_time_varying_k(desired_state, robot_state, v0=7, beta=0.5)
+            current_input = k * (robot_state - obstacle_center_point)
+        
+        elif np.linalg.norm(robot_state - obstacle_center_point) >= d_safe + eps:
+            k = calculate_time_varying_k(desired_state, robot_state, v0=10, beta=0.5)
+            current_input = k * (desired_state - robot_state)
+            
         # record the computed input at time-step t
         input_history[it] = current_input
 
@@ -93,9 +71,6 @@ def simulate_control():
             sim_visualizer.update_time_stamp( it*Ts )
             sim_visualizer.update_goal( desired_state )
             sim_visualizer.update_trajectory( state_history[:it+1] ) # up to the latest data
-            # update sensor visualization
-            pl_sens.set_data(obst_points[0], obst_points[1])
-            for i in range(len(sensors_dist)): pl_txt[i].set_position((obst_points[0,i], obst_points[1,i]))
         
         #--------------------------------------------------------------------------------
         # Update new state of the robot at time-step t+1
@@ -103,13 +78,15 @@ def simulate_control():
         robot_state = robot_state + Ts*current_input # will be used in the next iteration
         robot_state[2] = ( (robot_state[2] + np.pi) % (2*np.pi) ) - np.pi # ensure theta within [-pi pi]
 
-        # Update desired state if we consider moving goal position
-        #desired_state = desired_state + Ts*(-1)*np.ones(len(robot_state))
-
     # End of iterations
     # ---------------------------
     # return the stored value for additional plotting or comparison of parameters
     return state_history, goal_history, input_history
+
+
+def calculate_time_varying_k(desired_state, robot_state, v0, beta):
+    error = desired_state - robot_state
+    return (v0 * (1 - math.exp(-beta * np.linalg.norm(error)))) / np.linalg.norm(error)
 
 
 if __name__ == '__main__':
@@ -123,14 +100,14 @@ if __name__ == '__main__':
     t = [i*Ts for i in range( round(t_max/Ts) )]
 
     # # Plot historical data of control input
-    # fig2 = plt.figure(2)
-    # ax = plt.gca()
-    # ax.plot(t, input_history[:,0], label='vx [m/s]')
-    # ax.plot(t, input_history[:,1], label='vy [m/s]')
-    # ax.plot(t, input_history[:,2], label='omega [rad/s]')
-    # ax.set(xlabel="t [s]", ylabel="control input")
-    # plt.legend()
-    # plt.grid()
+    fig2 = plt.figure(2)
+    ax = plt.gca()
+    ax.plot(t, input_history[:,0], label='vx [m/s]')
+    ax.plot(t, input_history[:,1], label='vy [m/s]')
+    ax.plot(t, input_history[:,2], label='omega [rad/s]')
+    ax.set(xlabel="t [s]", ylabel="control input")
+    plt.legend()
+    plt.grid()
 
     # Plot historical data of state
     fig3 = plt.figure(3)
