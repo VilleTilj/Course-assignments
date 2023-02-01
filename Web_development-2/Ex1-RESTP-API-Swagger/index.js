6eb729872e2a9d16645429bb6c9583633a05acce
Ville Tiljander <ville.tiljander123@gmail.com>
2023-02-01 10:57:32 +0200

    Added get and post requests for building model

--- Web_development-2/Ex1-RESTP-API-Swagger/index.js
+++ Web_development-2/Ex1-RESTP-API-Swagger/index.js
@@ -1,14 +1,80 @@
 const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
-const port = 3000;                  // Save the port number where the server is listening
+const port = 8000;                  // Save the port number where the server is listening
 
 const bodyParser = require('body-parser'); //Makes parsin the request easier
 app.use(bodyParser.json()); //Set application/json as the default
+const fs = require('fs')
 
-
-let building = require('./data/building.json');
+let buildings = require('./data/building.json');
 let sensors = require('./data/sensors.json');
 
+
+const AddEndpointRules = {
+    id: 'id',
+    name: 'name',
+    address: 'address',
+    street_address: 'street_address',
+    post_number: "post_number"
+}
+
+
+function matches(body, rules, type) {
+    var keys = Object.keys(body);
+    var innerKeys = Object.keys(body['address']);
+    let i = 0;
+    let j = 0;
+    // Loop through building JSON format rules
+    for (let attribute in rules) {
+        if (i > 2 && type == "building") {
+            if (innerKeys[j] !== rules[attribute]) {
+                return false
+            }
+            j += 1;
+        }
+        else if (i <= 2 && keys[i] !== rules[attribute]) {
+            return false;
+        }
+        i += 1;
+    }
+    // All checked out, request body is OK
+    return true;
+}
+
+
+// Building API methods
+app.get('/buildings', (request, response) => {
+    response.status(200).json(buildings)
+});
+
+app.get('/buildings/:id', (request, response) => {
+    let id = request.params.id;
+    let building = buildings.filter(building => building.id === id);
+    if ( building.length !== 0) {
+        response.status(200).json(building)
+    } else {
+        response.status(404).send()
+    }
+});
+
+app.post('/buildings', (request, response) => {
+    let newBuilding = request.body; // Assume that the body contains json object
+    if (!matches(newBuilding, AddEndpointRules, "building" )) {
+        console.log("Request body is invalid")
+        response.status(400).send()
+    } else {
+        console.log(newBuilding)
+        newBuilding.id = buildings.length + 1;
+        // TODO add also to the file
+        buildings.push(newBuilding)
+        fs.writeFile('./data/building.json', JSON.stringify(buildings), 'utf-8',  function(err) {
+            if (err) throw err;
+            console.log('complete');
+            }
+        );
+        response.status(201).json(newBuilding)
+    }
+});
 
 // Start server
 const server = app.listen(port, () => {
